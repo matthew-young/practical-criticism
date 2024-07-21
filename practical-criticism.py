5c362d6cefcf15c88380f967c5350f0e21b95120
Matthew <matthewedyoung@gmail.com>
2024-07-21 19:23:11 -0300

    finished web app

--- practical-criticism.py
+++ practical-criticism.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 
 def get_random_poem():
     # Read the CSV file
-    df = pd.read_csv('PoetryFoundationData.csv')
+    df = pd.read_csv('kaggle_poem_dataset.csv')
     # Choose a random poem
     random_poem = df.sample(n=1).iloc[0]
     return {
         'title': random_poem['Title'],
-        'author': random_poem['Poet'],
-        'content': random_poem['Poem']
+        'author': random_poem['Author'],
+        'content':random_poem['Content'].replace('&amp;', '&')
     }
 
 @app.route('/')
